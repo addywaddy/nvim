78e3df75caee9f243299b8e13bc2794646c9e858
Adam Groves <adam.groves@share-now.com>
2025-11-11 08:19:41 +0100

    elixir lsp should be resolvable now

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,7 @@ return {
   { "williamboman/mason.nvim",           enabled = false },
   {
     "neovim/nvim-lspconfig",
+    dependencies = { "carldaws/miser.nvim" },
     opts = {
       servers = {
         ruby_lsp = {
@@ -12,7 +13,15 @@ return {
           mason = false,
         },
         elixirls = {
-          cmd = "elixir-ls",
+          cmd = (function()
+            local path = vim.fn.system("mise which elixir-ls 2>/dev/null"):gsub("%s+", "")
+            if vim.v.shell_error == 0 and path ~= "" then
+              return { path }
+            else
+              -- Fallback to PATH if mise fails
+              return { "elixir-ls" }
+            end
+          end)(),
           mason = false,
         },
         erlangls = {
